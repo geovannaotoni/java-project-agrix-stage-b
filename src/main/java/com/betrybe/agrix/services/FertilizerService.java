f14141087423c79369fbc19d435dcd82748cf719
Geovanna Otoni <geovannaotoni@gmail.com>
2023-12-27 09:26:41 -0300

    feat: controller and service for Fertilizer

---
+++ src/main/java/com/betrybe/agrix/services/FertilizerService.java
@@ -0,0 +1,21 @@
+package com.betrybe.agrix.services;
+
+import com.betrybe.agrix.models.entities.Fertilizer;
+import com.betrybe.agrix.models.repositories.FertilizerRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ * The type Fertilizer service.
+ */
+@Service
+public class FertilizerService {
+  private FertilizerRepository fertilizerRepository;
+
+  public FertilizerService(FertilizerRepository fertilizerRepository) {
+    this.fertilizerRepository = fertilizerRepository;
+  }
+
+  public Fertilizer createFertilizer(Fertilizer fertilizer) {
+    return fertilizerRepository.save(fertilizer);
+  }
+}
