30eabc6690f49cd9cb0199b8d22816b7c2dadf93
Geovanna Otoni <geovannaotoni@gmail.com>
2023-12-27 09:31:21 -0300

    feat: endpoint GET /fertilizers

--- src/main/java/com/betrybe/agrix/controllers/FertilizerController.java
+++ src/main/java/com/betrybe/agrix/controllers/FertilizerController.java
@@ -3,9 +3,11 @@ package com.betrybe.agrix.controllers;
 import com.betrybe.agrix.controllers.dto.FertilizerDto;
 import com.betrybe.agrix.models.entities.Fertilizer;
 import com.betrybe.agrix.services.FertilizerService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +30,18 @@ public class FertilizerController {
   public ResponseEntity<FertilizerDto> createFertilizer(@RequestBody FertilizerDto fertilizerDto) {
     Fertilizer newFertilizer = fertilizerService.createFertilizer(fertilizerDto.toFertilizer());
     return ResponseEntity.status(HttpStatus.CREATED).body(FertilizerDto.toDto(newFertilizer));
+  }
+
+  /**
+   * Gets all fertilizers.
+   *
+   * @return the all fertilizers
+   */
+  @GetMapping
+  public ResponseEntity<List<FertilizerDto>> getAllFertilizers() {
+    List<Fertilizer> fertilizers = fertilizerService.getAllFertilizers();
+    List<FertilizerDto> fertilizerDtos = fertilizers.stream()
+        .map(fertilizer -> FertilizerDto.toDto(fertilizer)).toList();
+    return ResponseEntity.ok(fertilizerDtos);
   }
 }
