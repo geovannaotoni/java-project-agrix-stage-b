23659eaf0c6510030016df90c5983524f0187571
Geovanna Otoni <geovannaotoni@gmail.com>
2023-12-27 09:38:36 -0300

    feat: endpoint GET /fertilizers/{id}

--- src/main/java/com/betrybe/agrix/controllers/FertilizerController.java
+++ src/main/java/com/betrybe/agrix/controllers/FertilizerController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,5 +44,18 @@ public class FertilizerController {
     List<FertilizerDto> fertilizerDtos = fertilizers.stream()
         .map(fertilizer -> FertilizerDto.toDto(fertilizer)).toList();
     return ResponseEntity.ok(fertilizerDtos);
+  }
+
+  /**
+   * Gets fertilizer by id.
+   *
+   * @param fertilizerId the fertilizer id
+   * @return the fertilizer by id
+   */
+  @GetMapping("/{fertilizerId}")
+  public ResponseEntity<FertilizerDto> getFertilizerById(@PathVariable Long fertilizerId) {
+    Fertilizer fertilizer = fertilizerService.getFertilizerById(fertilizerId);
+    FertilizerDto fertilizerDto = FertilizerDto.toDto(fertilizer);
+    return ResponseEntity.ok(fertilizerDto);
   }
 }
--- src/main/java/com/betrybe/agrix/services/FertilizerService.java
+++ src/main/java/com/betrybe/agrix/services/FertilizerService.java
@@ -1,8 +1,10 @@
 package com.betrybe.agrix.services;
 
+import com.betrybe.agrix.exception.FertilizerNotFoundException;
 import com.betrybe.agrix.models.entities.Fertilizer;
 import com.betrybe.agrix.models.repositories.FertilizerRepository;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 /**
@@ -22,5 +24,19 @@ public class FertilizerService {
 
   public List<Fertilizer> getAllFertilizers() {
     return fertilizerRepository.findAll();
+  }
+
+  /**
+   * Gets fertilizer by id.
+   *
+   * @param fertilizerId the fertilizer id
+   * @return the fertilizer by id
+   */
+  public Fertilizer getFertilizerById(Long fertilizerId) {
+    Optional<Fertilizer> fertilizer = fertilizerRepository.findById(fertilizerId);
+    if (fertilizer.isEmpty()) {
+      throw new FertilizerNotFoundException();
+    }
+    return fertilizer.get();
   }
 }
